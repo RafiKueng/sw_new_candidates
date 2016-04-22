5190277df277860e5fd63fde3728aae552f65afc
Rafael Kueng <rafik@physik.uzh.ch>
2016-04-22 17:41:36 +0200

    restructure add central selection

--- batch_do_nsynth_img.py
+++ batch_do_nsynth_img.py
@@ -57,7 +57,7 @@ class RectCoords(object):
         self.x1 = x1
         self.y1 = y1
         
-    def __repr__(self):
+    def __str__(self):
         return "RectCoords((%4i/%4i) to (%4i/%4i))" % (self.x0, self.y0, self.x1, self.y1)
         
     @property
@@ -146,6 +146,131 @@ _rectspec_neg = {
     'linewidth': 3.0       
 }
 
+
+class RectMask(RectCoords):
+    def __init__(self, xy0, xy1=None, kind=None):
+
+        if xy1 is None:
+            xy1 = (xy0[0]+1, xy0[1]+1)
+        # super(RectMask, self).__init__(x0,y0,x1,y1)
+
+        # init patch first, the other setters need it..
+        self.patch = mpl.patches.Rectangle((0,0), 1, 1, **_rectspec_pos) # real size will be set when assiging xy01
+        
+        if kind is None:
+            kind = '+'
+        self._kind = kind # '+': inclusive, '-' exclusive
+        
+        # set the corrdinates, that also sets the patch
+        self.xy0 = xy0
+        self.xy1 = xy1
+
+
+    def __str__(self):
+        return "RectMask%s (%4i/%4i to %4i/%4i)" % (self.kind, self.x0, self.y0, self.x1, self.y1)
+        
+    @property
+    def x0(self):
+        return self._x0
+
+    @x0.setter
+    def x0(self, v):
+        self._x0 = v
+        self.patch.set_x(v)
+        self.update()
+
+    @property
+    def y0(self):
+        return self._y0
+
+    @y0.setter
+    def y0(self, v):
+        self._y0 = v
+        self.patch.set_y(v)
+        self.update()
+
+    @property
+    def x1(self):
+        return self._x1
+
+    @x1.setter
+    def x1(self, v):
+        self._x1 = v
+        self.patch.set_width(self.dx)
+        self.update()
+
+    @property
+    def y1(self):
+        return self._y1
+
+    @y1.setter
+    def y1(self, v):
+        self._y1 = v
+        self.patch.set_height(self.dy)
+        self.update()
+
+    @property
+    def xy0(self):
+        return (self.x0, self.y0)
+
+    @property
+    def xy1(self):
+        return (self.x1, self.y1)
+
+    @xy0.setter
+    def xy0(self, v):
+        self.x0 = v[0]
+        self.y0 = v[1]
+
+    @xy1.setter
+    def xy1(self, v):
+        self.x1 = v[0]
+        self.y1 = v[1]
+
+    @property
+    def kind(self):
+        return self._kind
+
+    @kind.setter
+    def kind(self, v):
+        if v == '+':
+            self._kind = '+'
+            self.patch.set(**_rectspec_pos)
+        elif v == '-':
+            self._kind = '-'
+            self.patch.set(**_rectspec_neg)
+        
+    def update(self):
+        pass
+    
+    def switch_kind(self):
+        if self.kind == '+':
+            self.kind = '-'
+        else:
+            self.kind = '+'
+
+
+
+class GlobalHotkeyHandler(object):
+    
+    def __init__(self, fig):
+        self.fig = fig
+        self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+        self.fig.canvas.mpl_connect('key_release_event', self.on_key_release)
+    
+    def on_key_press(self, event):
+        log.debug("key press global '%s'", event.key)
+    
+    def on_key_release(self, event):
+        log.debug("key release global '%s'", event.key)
+        
+        if event.key == "escape":
+            plt.close(self.fig)
+        
+
+
+        
+
 class MasksHandler(object):
     
     def __init__(self, ax=None):
@@ -153,18 +278,19 @@ class MasksHandler(object):
         self.ax = ax if ax is not None else plt.gca()
         self.update_handler = []
         self.register(self.selfupdate)
+        self.mode = '+' # are we masking in [+], masking out [-] or setting the center[.]
+        # update: selecting the center can be done al the time using right click
         
-        self.patch = None # holds the current rect
-        self.c = None
-        self.patchdata = {}
+        self.masks = []
+        self.selected_mask = None
 
-        self.patches = [] # holds a list of all tects
+        self.center = None        
+        self.cxl = None # center x line
+        self.cyl = None
         
-        self.add_rect()
-        
-        self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
-        self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_move)
-        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.ax.figure.canvas.mpl_connect('button_press_event', self.on_mouse_click)
+        self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
+        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_mouse_release)
         # self.ax.figure.canvas.mpl_connect('key_press_event', self.on_key_press)
         self.ax.figure.canvas.mpl_connect('key_release_event', self.on_key_release)
 
@@ -178,15 +304,8 @@ class MasksHandler(object):
             h()
 
     def selfupdate(self):
-        self.patch.set_width(self.c.dx)
-        self.patch.set_height(self.c.dy)
-        self.patch.set_xy((self.c.x0, self.c.y0))
-        
         self.ax.figure.canvas.draw()
-#        self.ax.draw_artist(self.patch)
-#        for patch in self.patches:
-#            print patch[1]
-#            self.ax.draw_artist(patch[1])
+
 
     def save(self):
         D = []
@@ -202,200 +321,132 @@ class MasksHandler(object):
         self.selfupdate()
             
 
-    def add_rect(self, t='incl', c=RectCoords(0,0,0,0), patchdata=None):
+    def add_mask(self, xy0, xy1=None):
+        if xy1 is None:
+            xy1 = (xy0[0]+1, xy0[1]+1)
+        log.debug("add mask xy0=%s xy1=%s", xy0, xy1)
+        
+        mask = RectMask(xy0, xy1, self.mode)
+        
+        self.masks.append(mask)
+        self.selected_mask = mask
 
-        # set edge color depending on type
-        if t=='incl':
-            ec = _rectspec_pos
-        elif t=='excl':
-            ec = _rectspec_neg
-        
-        self.c = c
-        self.patch = mpl.patches.Rectangle((0,0), 1, 1, **ec)
-        if patchdata:
-            self.patchdata = patchdata
-        else:
-            self.patchdata = {'type':t}
-        
-        self.patches.append((self.c, self.patch, self.patchdata))
-        self.ax.add_patch(self.patch)
-        #self.selfupdate()
+        self.ax.add_patch(mask.patch)
+        self.ax.draw_artist(mask.patch)
 
-    def remove_rect(self):
-        log.debug("removing last rect and selecting second last")
-        c, patch, patchdata = self.patches.pop()
-        patch.remove()
-        del c, patchdata, patch
-        
-        if len(self.patches)>=1:
-            self.c, self.patch, self.patchdata = self.patches[-1]
+
+    def remove_mask(self, i=None):
+        log.debug("removing mask index: %s", i)
+
+        if i is None:
+            mask = self.selected_mask
         else:
-            self.add_rect()
-        self.update()
+            mask = self.masks[i]
+            
+
+        self.masks.remove(mask)
         
-    def select_rect(self, i):
-        log.debug("selecting rect %i", i)
-        if len(self.patches)>=i:
-            self.c, self.patch, self.patchdata = self.patches[0]
+        mask.patch.remove()
+        del mask
+        
+        if len(self.masks)>0:
+            self.selected_mask = self.masks[-1]
+        else:
+            self.selected_mask = None
+            
+            
+    def set_center(self, xy):
+        log.debug("set center %s", xy)
+        self.center = xy
+        if self.cxl is None:
+            self.cxl = self.ax.axvline(xy[0], color="y")
+            self.cyl = self.ax.axhline(xy[1], color="y")
+        else:
+            self.cxl.set_xdata(xy[0])
+            self.cyl.set_ydata(xy[1])
+
+
+    def switch_mode(self, mode):
+        log.debug("switching mode to %s", mode)
+        # mode = {'+':'mask+','-':'mask-','.':'center'}[smode]
+        self.mode = mode
+        
+        if mode=="+":
+            self.selected_mask.kind = '+'
+
+        elif mode=="-":
+            self.selected_mask.kind = '-'
+
+        elif mode==".":
+            pass
+        
 
 
     def on_key_release(self, event):
-        log.debug('keypress (%s)', event.key)
+        log.debug('key release (%s)', event.key)
         
-        if event.key=="+":
-            self.add_rect('incl')
+        if event.key in ['+', '-', '.']:
+            self.switch_mode(event.key)
 
-        elif event.key=="-":
-            self.add_rect('excl')
+        elif event.key=="insert":
+            self.add_mask()
         
-        elif event.key=="enter":
-            self.remove_rect()
+        elif event.key=="delete":
+            self.remove_mask()
             
         else:
             try:
                 i = int(event.key)
             except:
                 return
-            self.select_rect(i)
-            
+            if i>=len(self.masks):
+                i = len(self.masks)
+            self.selected_mask = self.masks[i]
         
 
-    def on_press(self, event):
-        log.debug( 'mouse btn press (%s %s %s)', event.inaxes, event.button, event.key)
+    def on_mouse_click(self, event):
+        log.debug( 'mouse click (%s %s %s)', event.inaxes, event.button, event.key)
+
         if event.inaxes == self.ax:
+            e_xy = (event.xdata, event.ydata)
+
             if event.button == 1:
-                self.c.x0 = event.xdata
-                self.c.y0 = event.ydata
+                if event.key == "shift": # use shift to edit existing
+                    self.selected_mask.xy0 = e_xy
+                else:
+                    self.add_mask(e_xy)
+
+            elif event.button == 2:
+                self.remove_mask()
+
+            elif event.button == 3:
+                self.set_center(e_xy)
+                pass
 
 
-    def on_move(self, event):
+    def on_mouse_move(self, event):
+        
         if event.inaxes == self.ax:
 
             if event.button==1:
                 log.debug( 'mouse move (%s %s %s)', event.inaxes, event.button, event.key)
-                if self.c.x0 is None:
-                    log.warn("use rightclick to first create a rect before moving it")
-                    return
+                
+                #if self.c.x0 is None:
+                #    log.warn("use rightclick to first create a rect before moving it")
+                #    return
+                    
+                self.selected_mask.x1 = event.xdata
+                self.selected_mask.y1 = event.ydata
 
-                self.c.x1 = event.xdata
-                self.c.y1 = event.ydata
-                #self.update_patch()
-                self.selfupdate()
+                self.ax.draw_artist(self.selected_mask.patch)
 
-    def on_release(self, event):
+
+    def on_mouse_release(self, event):
         log.debug( 'mouse btn release (%s %s %s)', event.inaxes, event.button, event.key)
 #        if event.inaxes == self.ax:
 #            self.update()
 
-#
-#_zoomrectspec = {
-#    'edgecolor': 'y',
-#    'facecolor': None,
-#    'fill': False,
-#    'linewidth': 1.0       
-#}
-#
-#class ZoomHandler(object):
-#    def __init__(self, ax1, ax2):
-#        
-#        self.ax1 = ax1 
-#        self.ax2 = ax2 # the ax that shows the zoomed window
-#        
-#        log.debug( "ax1: %s ax2 %s", ax1, ax2)
-#        
-#        self.c = RectCoords()
-#        
-#        self.quadratic = False
-#        
-#        self.rect = mpl.patches.Rectangle((0,0), 1, 1, **_zoomrectspec)
-#        self.ax1.add_patch(self.rect)
-#        
-#        self.ax1.figure.canvas.mpl_connect('button_press_event', self.on_press)
-#        self.ax1.figure.canvas.mpl_connect('motion_notify_event', self.on_move)
-#
-#        self.ax1.figure.canvas.mpl_connect('scroll_event', self.on_scroll)
-#
-#        self.ax1.figure.canvas.mpl_connect('key_press_event', self.on_key_press)
-#        self.ax1.figure.canvas.mpl_connect('key_release_event', self.on_key_release)
-#
-#
-#    def update_rect(self):
-#        self.rect.set_width(self.c.width)
-#        self.rect.set_height(self.c.height)
-#        self.rect.set_xy((self.c.x0, self.c.y0))
-#        #self.ax1.figure.canvas.draw()
-#        self.ax1.draw_artist(self.rect)
-#        self.update_zoomed_view()
-#
-#    def update_zoomed_view(self):
-#        self.ax2.set_xlim(self.c.xlim)
-#        self.ax2.set_ylim(self.c.ylim)
-#        self.ax2.figure.canvas.draw()
-#
-#    def on_key_press(self, event):
-#        log.debug('keypress (%s)', event.key)
-#        if event.inaxes == self.ax1:
-#            if event.key == "control":
-#                self.quadratic = True
-#    
-#    def on_key_release(self, event):
-#        log.debug( 'keyrelease (%s)', event.key)
-#        if event.inaxes == self.ax1:
-#            if event.key == "control":
-#                self.quadratic = False
-#
-#    def on_press(self, event):
-#        log.debug( 'btn press (%s %s)', event.inaxes, event.button)
-#        if event.inaxes == self.ax1:
-#            if event.button == 1: #left: set middle
-#                pass
-#            
-#            elif event.button == 3: # right button: draw area
-#                self.c.x0 = event.xdata
-#                self.c.y0 = event.ydata
-#                
-#    def on_move(self, event):
-#        if event.inaxes == self.ax1:
-#            ex = event.xdata
-#            ey = event.ydata
-#                
-#            if event.button == 1: #left: set middle
-#                log.debug( 'mouse move (%s %s %s)', event.inaxes, event.button, event.key)
-#                if self.c.x0 is None:
-#                    log.warn("use rightclick to first create a rect before moving it")
-#                    return
-#                self.c.shift_to(ex, ey)
-#                self.update_rect()
-#                
-#            elif event.button == 3: # right button: draw area
-#                log.debug( 'mouse move (%s %s %s)', event.inaxes, event.button, event.key)
-#                if self.quadratic:
-#                    dx = ex - self.c.x0
-#                    dy = ey - self.c.y0
-#                    dd = max(abs(dx),abs(dy))
-#                    ex = self.c.x0 + dd * np.sign(dx)
-#                    ey = self.c.y0 + dd * np.sign(dy)
-#                self.c.x1 = ex
-#                self.c.y1 = ey
-#                self.update_rect()
-#
-#
-#    def on_scroll(self, event):
-#        log.debug('scroll (%s %s %s)', event.inaxes, event.button, event.key)
-#        if event.inaxes == self.ax1:
-#                
-#            if event.button == 'up':
-#                print "up"
-#                self.c.zoom(0.1)
-#                self.update_rect()
-#                
-#            elif event.button == 'down':
-#                print "up"
-#                self.c.zoom(-0.1)
-#                self.update_rect()
-#            
 
-    
     
 class RectangleZoomSelector(object):
     
@@ -452,11 +503,11 @@ class RectangleZoomSelector(object):
 
 class ROIDisplay(object):
     
-    def __init__(self, ax, size=[1,1], roi=None, patches=None):
+    def __init__(self, ax, size=[1,1], roi=None, masks=None):
         self.ax = ax
         self.size = size
         self.roi = roi
-        self.patches = patches
+        self.masks = masks
         self.mask = np.zeros(size, dtype=np.uint8)
         
         self.imgObj = ax.imshow(self.mask, cmap='gray', interpolation="none")
@@ -469,11 +520,14 @@ class ROIDisplay(object):
         
     def selfupdate(self):
         log.debug("ROI selfupdate")
+        
         x0,x1 = self.roi.xlim
         y0,y1 = self.roi.ylim
         
-        incl = [c for c,p,d in self.patches if d['type']=='incl']
-        excl = [c for c,p,d in self.patches if d['type']=='excl']
+        incl = [m for m in self.masks if m.kind == '+']
+        excl = [m for m in self.masks if m.kind == '-']
+#        incl = [c for c,p,d in self.patches if d['type']=='incl']
+#        excl = [c for c,p,d in self.patches if d['type']=='excl']
 
         print incl
         print excl
@@ -481,15 +535,10 @@ class ROIDisplay(object):
         self.mask[:,:] = 0
         
         # make sure to first include, then exclude again
-        for coords, b in [(incl, 255),(excl, 128)]:
-            for c in coords:
-#                x0 = c.x0-self.roi.x0
-#                if x0<=0: x0= 0
-#                y0 = c.y0-self.roi.y0
-#                if y0<=0: y0= 0
-#                x1 = c.x1-self.roi.x0 if c.x1<self.roi.x1 else self.roi.width
-#                y1 = c.y1-self.roi.x0 if c.y1<self.roi.y1 else self.roi.height
-                self.mask[c.slice2D] = b
+        for masks, v in [(incl, 255),(excl, 128)]:
+            for mask in masks:
+                log.debug("ROI drawing, %s %s", v, mask)
+                self.mask[mask.slice2D] = v
                 
         self.imgObj.set_data(self.mask)
         self.ax.set_xlim(self.roi.xlim)
@@ -532,33 +581,16 @@ for asw in data:
         ax2.xaxis.set_visible(False)
         ax2.yaxis.set_visible(False)
     
-
+    hotkeys = GlobalHotkeyHandler(fig)
     h_c = mpl.widgets.MultiCursor(fig.canvas, (ax1, ax2, ax3), color='r', lw=1, horizOn=True, vertOn=True,useblit=False)
 
     #h_z = ZoomHandler(ax1,ax2)
     zoomsel = RectangleZoomSelector(ax1,ax2)
     masksel = MasksHandler(ax2)
-    maskshow = ROIDisplay(ax3, orgimg.shape, zoomsel.c, masksel.patches)
+    maskshow = ROIDisplay(ax3, orgimg.shape, zoomsel.c, masksel.masks)
 
     zoomsel.register(masksel.update)
     #masksel.register(maskshow.update)
-    
-    
-#    rectsel = matplotlib.widgets.RectangleSelector(
-#        ax1, onselect,
-#        drawtype='box',
-#        minspanx=None,
-#        minspany=None,
-#        useblit=False,
-#        lineprops=None,
-#        rectprops=None,
-#        spancoords='data',
-#        button=None,
-#        maxdist=10,
-#        marker_props=None,
-#        interactive=True,
-#        #state_modifier_keys=None
-#        )
     
     
     fig.show()
