3fae4062c59c8a76744e8088dfe62c651c25fe61
Prasenjit Saha <psaha@physik.uzh.ch>
2016-07-07 15:42:15 +0200

    mosterinv function

--- moster.py
+++ moster.py
@@ -1,22 +1,34 @@
 # Moster et al (2010)
 # http://adsabs.harvard.edu/abs/2010ApJ...710..903M
 
-#import numpy as np
-#import matplotlib.pyplot as pl
+from scipy.optimize import brentq
 
 M1 = 10**(11.884)
 mr = 0.02820
 beta = 1.057
 gamma = 0.556
 
-# M = np.logspace(11,14,20)
-
-# m = M*2*mr / ((M/M1)**(-beta) + (M/M1)**gamma)
-
-# pl.loglog(m,M)
-# pl.show()
-
 def moster(M):
     m = M*2*mr / ((M/M1)**(-beta) + (M/M1)**gamma)
     return m
-    
+
+# brentq(moster(M), -pi, pi)
+
+def mosterinv(m):
+    return brentq(lambda M: moster(M) - m, 1e11, 1e14)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    import matplotlib.pyplot as pl
+
+    M = np.logspace(11,14,20)
+    m = M*2*mr / ((M/M1)**(-beta) + (M/M1)**gamma)
+    Mx = np.array([mosterinv(x) for x in m])
+    pl.loglog(m,M)
+    pl.loglog(m,2*Mx)
+
+    pl.show()
+
+
+
