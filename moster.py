e222dc0c32c547831598e99dc35780303b0fbe5d
Rafael Kueng <rafik@physik.uzh.ch>
2016-07-26 15:04:38 +0200

    kappa map bugfixes

--- moster.py
+++ moster.py
@@ -1,6 +1,7 @@
 # Moster et al (2010)
 # http://adsabs.harvard.edu/abs/2010ApJ...710..903M
 
+import numpy as np
 from scipy.optimize import brentq
 
 M1 = 10**(11.884)
@@ -14,17 +15,17 @@ def moster(M):
 
 # brentq(moster(M), -pi, pi)
 
-def mosterinv(m):
-    return brentq(lambda M: moster(M) - m, 1e11, 1e14)
+def inv(m):
+    #print "  mos_inv:", np.log10(m), np.log10(moster(m)), np.log10(m-moster(m))
+    return brentq(lambda M: moster(M) - m, 1e9, 1e15)
 
 
 if __name__ == '__main__':
-    import numpy as np
     import matplotlib.pyplot as pl
 
     M = np.logspace(11,14,20)
     m = M*2*mr / ((M/M1)**(-beta) + (M/M1)**gamma)
-    Mx = np.array([mosterinv(x) for x in m])
+    Mx = np.array([inv(x) for x in m])
     pl.loglog(m,M)
     pl.loglog(m,2*Mx)
 
